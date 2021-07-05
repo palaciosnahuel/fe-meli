bfd24a3b6412bf1eeb4c1713f237f2c527cfa04b
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-05 17:31:09 -0300

    Eliminado uso de dependencia innecesario

--- front/src/Components/navbar/Navbar.js
+++ front/src/Components/navbar/Navbar.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './Navbar.scss';
 import Searchbox from '../searchbox/Searchbox';
-import useState from 'react';
 
 const Navbar = () => {
-
     return (
         <div className='navbar'>
            <Searchbox/>
