3f256e13e3956b93e2f683d39147ac7823b0acc0
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-05 11:50:45 -0300

    Add main styles and files scss.

--- front/src/App.js
+++ front/src/App.js
@@ -2,17 +2,19 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Navbar from "./Components/navbar/Navbar";
 import Productlist from './Components/productlist/Productlist';
 import ProductItem from "./Components/ProductItem/ProductItem";
+import './Styles/App.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="main">
+      <BrowserRouter>
         <Navbar/>
         <Switch>
           <Route exact path='/items' component={Productlist} />
           <Route path='/items/:id'component={ProductItem} />
         </Switch>
       </BrowserRouter>
-
+    </div>
   );
 }
 
