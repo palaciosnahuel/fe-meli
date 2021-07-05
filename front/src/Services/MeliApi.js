1f344103a33ebfebcd14dda503d7c2579339ad94
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-05 13:01:32 -0300

    Agregada funcion getProduct, para obtener el producto con descripcion

--- front/src/Services/MeliApi.js
+++ front/src/Services/MeliApi.js
@@ -7,3 +7,12 @@ export const getProducts = async (query,limit=4) => {
     } catch (err) {}
   };
   
+  export const getProduct = async (query) => {
+    try {
+      let url = `http://localhost:5000/api/items/${query}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (err) {}
+  };
+  
