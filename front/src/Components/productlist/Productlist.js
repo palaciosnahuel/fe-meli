ba765b13ebf6afeadfd0ce81de2bc24aa397c407
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-05 22:03:10 -0300

    Agregado Breadcrumb a ProductList

--- front/src/Components/productlist/Productlist.js
+++ front/src/Components/productlist/Productlist.js
@@ -3,26 +3,32 @@ import './Productlist.scss';
 import ProductItem from '../ProductItem/ProductItem';
 import {getProducts} from '../../Services/MeliApi';
 import {useLocation } from "react-router-dom";
+import Breadcrumb from '../Breadcrumb/Breadcrumb';
+
 
 const Productlist = () => {
 
     const [productos, setProductos] = useState([]);
+    const [categories, setCategories] = useState([]);
+
     const location = useLocation();
     let query = new URLSearchParams(location.search).get("search");
 
     useEffect(() => {
         getProducts(query).then((response)=>{ 
             setProductos(response);
+            setCategories(response.categories);
             document.title = `${query} | MercadoLibre.com.ar`;
         });;
       }, [location]);
 
-
     return (
-        <ol className="product-container">
-           {productos.items ? 
-                productos.items.map((item, index) => <ProductItem data={item} key={index}/>) : (<div> Cargando</div>)}
-        </ol>
+        <div>
+            {categories ? <Breadcrumb categories={ categories }/> : ""}
+            <ol className="product-container">
+                {productos.items ? productos.items.map((item, index) => <ProductItem data={item} key={index}/>) : (<div> Cargando</div>)}
+            </ol>
+        </div>
     )
 }
 
