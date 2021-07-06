46ae808000398fb61f6818ef8fd1adc1f678137f
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-05 22:01:58 -0300

    Modificados stylos de ProductItem

--- front/src/Components/ProductItem/ProductItem.js
+++ front/src/Components/ProductItem/ProductItem.js
@@ -5,28 +5,27 @@ import TruckIcon from '../../Assets/ic_shipping.png';
 
 
 const ProductItem = (data) => {
-    const history = useHistory();
 
+    const history = useHistory();
     const handleClick = (id) => { history.push({ pathname: `/items/${id}`}); }
 
     return (
-        <li className="product-item-card">
-            
-            <div className="product-item-image" onClick={()=>handleClick(data.data.id)} >
-                <img src={data.data.picture} alt={data.title} />
-            </div>
-            <div className="product-item-body">
-                <div className="product-item-price">${data.data.price.amount}
+            <li className="product-item-card">
                 
-                {data.data.free_shipping &&
-                <img src={TruckIcon} alt='Free shipping available' className='shipping-icon' />
-                }
+                <div className="product-item-image" onClick={()=>handleClick(data.data.id)} >
+                    <img src={data.data.picture} alt={data.title} />
                 </div>
-                <span className="product-item-address">{data.data.address.state_name}</span>
-                <span className="product-item-title" onClick={()=>handleClick(data.data.id)}>{data.data.title}</span>
-            </div>
-            
-        </li>
+                <div className="product-item-body">
+                    <div className="product-item-price">$ {data.data.price.amount}
+                    
+                    {data.data.free_shipping &&
+                    <img src={TruckIcon} alt='Free shipping available' className='shipping-icon' />
+                    }
+                    </div>
+                    <span className="product-item-address">{data.data.address.state_name}</span>
+                    <span className="product-item-title" onClick={()=>handleClick(data.data.id)}>{data.data.title}</span>
+                </div>
+            </li>
     )
 }
 
