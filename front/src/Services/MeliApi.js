a6c6411125e495368e38f87443c7da9ad2d59a2c
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-08 15:07:38 -0300

    Catch error

--- front/src/Services/MeliApi.js
+++ front/src/Services/MeliApi.js
@@ -13,10 +13,17 @@ export const getProducts = async (query,limit=4) => {
     try {
       let url = `http://localhost:5000/api/items/${query}`;
       const response = await fetch(url);
-      const data = await response.json();
-      return data;
+      if (response.status === 500) {
+        return response;
+      }
+      else {
+        const data = await response.json();
+        return data;
+      }
     } catch (err) {
       console.error(err);
     }
   };
+  
+
   
