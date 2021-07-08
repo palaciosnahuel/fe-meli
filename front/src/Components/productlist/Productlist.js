7cabcc98c83b994cc104b607425eafa1dca72dc7
nawi <nahuelpalaciossalguero@gmail.com>
2021-07-08 13:16:07 -0300

    Agregada vista cuando no hay resultados

--- front/src/Components/productlist/Productlist.js
+++ front/src/Components/productlist/Productlist.js
@@ -10,24 +10,33 @@ const ProductList = () => {
 
     const [productos, setProductos] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [resultsFound, setResultsFound] = useState(false);
 
     const location = useLocation();
     let query = new URLSearchParams(location.search).get("search");
 
     useEffect(() => {
         getProducts(query).then((response)=>{ 
-            setProductos(response);
+            setProductos(response.items);
+            (response.items.length !== 0 ) ? setResultsFound(true) : setResultsFound(false);
             setCategories(response.categories);
             document.title = `${query} | MercadoLibre.com.ar`;
-        });;
+        });
       }, [location]);
 
+
     return (
-        <div>   
-            {categories ? <div className="product-breadcrumb-container"><Breadcrumb categories={ categories }/>  </div> : ""}
-            <ol className="product-container">
-                {productos.items ? productos.items.map((item, index) => <ProductItem data={item} key={index}/>) : (<div> Cargando</div>)}
-            </ol>
+        
+        <div> 
+            {resultsFound ? 
+            <div> 
+                {categories ? <div className="product-breadcrumb-container"><Breadcrumb categories={ categories }/>  </div> : ""}
+                <ol className="product-container">
+                    {productos ? productos.map((item, idx) => <ProductItem data={item} key={idx}/>) : (<div> Cargando</div>)}
+                </ol>
+            </div> 
+            : <div className="product-container"> Sin resultados </div>}
+            
         </div>
     )
 }
